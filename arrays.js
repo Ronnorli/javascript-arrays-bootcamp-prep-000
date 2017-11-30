f0afede200f6650895f3399cbf17ab078f09d5aa
Ronald Li <ronaldbrooklynli@gmail.com>
2017-11-30 01:23:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,37 +1,42 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-
-function addElementToBeginningOfArray(array, element) {
-  var newArray = [element, array[0]];
-  return newArray
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+//var candyString = "foo"
+ 
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
 }
-
-
-function descructivelyAddElementToBeginningOfArray(array, element){
-   array.unshift(element)
-  return array
- }
  
- function addElementToEndOfArray(array, element) {
-   array.push(element)
-   return array
- }
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
  
- function destructivelyAddElementToEndOfArray(array, element){
-   array.push(element)
-   return array
- }
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
  
- function accessElementInArray(array, index) {
-  		  
-   return array[2]
- }
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
  
- function removeElementFromBeginningOfArray(array) {
-   array.shift()
-   return array
- }
+function accessElementInArray(array, index){
+  return array[index];
+}
  
- function removeElementFromEndOfArray(array) {
-   array.pop()
-   return array
-  }		  
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+ 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+ 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+ 
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
